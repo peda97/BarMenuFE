59633288a109c59a5777efed6938a0499660fc0a
Eda Palushaj <palushaj.eda97@gmail.com>
2023-03-11 18:19:19 +0100

    feat(dev-bar-menu): products & receipt component created

--- src/app/components/categories/categories.module.ts
+++ src/app/components/categories/categories.module.ts
@@ -6,11 +6,15 @@ import { CategoriesComponent } from './categories.component';
 import { CategoriesService } from 'src/app/services/categories.service';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { ProductsComponent } from './products/products.component';
+import { ReceiptComponent } from './products/receipt/receipt.component';
 
 
 @NgModule({
   declarations: [
-    CategoriesComponent
+    CategoriesComponent,
+    ProductsComponent,
+    ReceiptComponent
   ],
   imports: [
     CommonModule,
